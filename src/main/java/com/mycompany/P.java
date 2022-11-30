936f1094dd92775ad04f54313ff4b872b2032705
BuildTools <drew.escorcia@gmail.com>
2022-11-29 22:54:39 -0500

    new file:   src/main/java/com/mycompany/F.java 	new file:   src/main/java/com/mycompany/I.java 	new file:   src/main/java/com/mycompany/J.java 	new file:   src/main/java/com/mycompany/K.java 	new file:   src/main/java/com/mycompany/M.java 	new file:   src/main/java/com/mycompany/N.java 	new file:   src/main/java/com/mycompany/P.java 	new file:   src/main/java/com/mycompany/Q.java 	new file:   src/main/java/com/mycompany/S.java 	new file:   src/main/java/com/mycompany/T.java 	new file:   src/main/java/com/mycompany/U.java 	new file:   src/main/java/com/mycompany/V.java 	modified:   src/main/java/com/mycompany/WordtoLetter.java 	new file:   src/main/java/com/mycompany/X.java 	new file:   src/main/java/com/mycompany/Y.java 	new file:   src/main/java/com/mycompany/Z.java 	new file:   target/classes/com/mycompany/F.class 	new file:   target/classes/com/mycompany/I.class 	new file:   target/classes/com/mycompany/J.class 	new file:   target/classes/com/mycompany/K.class 	new file:   target/classes/com/mycompany/M.class 	new file:   target/classes/com/mycompany/N.class 	new file:   target/classes/com/mycompany/P.class 	new file:   target/classes/com/mycompany/Q.class 	new file:   target/classes/com/mycompany/S.class 	new file:   target/classes/com/mycompany/T.class 	new file:   target/classes/com/mycompany/U.class 	new file:   target/classes/com/mycompany/V.class 	modified:   target/classes/com/mycompany/WordtoLetter.class 	new file:   target/classes/com/mycompany/X.class 	new file:   target/classes/com/mycompany/Y.class 	new file:   target/classes/com/mycompany/Z.class

---
+++ src/main/java/com/mycompany/P.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany;
+
+import javafx.animation.TranslateTransition;
+import static javafx.scene.paint.Color.rgb;
+import javafx.scene.text.Font;
+
+/**
+ *
+ * @author Andrew
+ */
+public class P extends Letters {
+    
+    public P(String size, int r, int g, int b,String font){
+        super.setSize(size);
+        super.setColor(new int[]{r,g,b});
+        super.setFont(font);
+        if(super.getSize().equalsIgnoreCase("upper")){
+            super.setCharater('P');
+        }
+        else if(super.getSize().equalsIgnoreCase("lower")){
+            super.setCharater('p');
+        
+        }
+        super.getLabel().setFont(Font.font(super.getFont(),50));
+        super.getLabel().setTextFill(rgb(super.getColor()[0],super.getColor()[1],super.getColor()[2]));
+        super.getLabel().setText(Character.toString(super.getCharater()));
+        
+    }
+    
+    @Override
+    public void anim(){
+        TranslateTransition p = new TranslateTransition();
+        p.setNode(super.getLabel());
+        p.setCycleCount(TranslateTransition.INDEFINITE);
+        p.setByX(5);
+        p.setAutoReverse(true);
+        p.play();
+    }
+    
+}
---
+++ src/main/java/com/mycompany/X.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany;
+
+import javafx.animation.TranslateTransition;
+import static javafx.scene.paint.Color.rgb;
+import javafx.scene.text.Font;
+
+/**
+ *
+ * @author Andrew
+ */
+public class X extends Letters {
+    
+    public X(String size, int r, int g, int b,String font){
+        super.setSize(size);
+        super.setColor(new int[]{r,g,b});
+        super.setFont(font);
+        if(super.getSize().equalsIgnoreCase("upper")){
+            super.setCharater('X');
+        }
+        else if(super.getSize().equalsIgnoreCase("lower")){
+            super.setCharater('x');
+        
+        }
+        super.getLabel().setFont(Font.font(super.getFont(),50));
+        super.getLabel().setTextFill(rgb(super.getColor()[0],super.getColor()[1],super.getColor()[2]));
+        super.getLabel().setText(Character.toString(super.getCharater()));
+        
+    }
+    
+    @Override
+    public void anim(){
+        TranslateTransition x = new TranslateTransition();
+        x.setNode(super.getLabel());
+        x.setCycleCount(TranslateTransition.INDEFINITE);
+        x.setByX(5);
+        x.setAutoReverse(true);
+        x.play();
+    }
+    
+}
